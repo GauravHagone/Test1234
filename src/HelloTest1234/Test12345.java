ac22d381830727eee08673626f55719914707c27
Gaurav Hagone <gkhagone@gmail.com>
2021-03-17 13:36:24 +0530

    my project

---
+++ src/HelloTest1234/Test12345.java
@@ -0,0 +1,9 @@
+package HelloTest1234;
+
+public class Test12345 {
+	public static void main(String[] args) {
+		// Comment: printing output on console
+		System.out.println("Hello World");
+		}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,3 @@
+module Test1234 {
+	
+}
